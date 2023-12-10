00e5db3f54e225e5a4c82184247a7af38eb505e2
Rachel Pratt <rachlpratt@gmail.com>
2023-12-10 14:24:33 -0500

    Add app.yaml file and deploy to GCP

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 books = "books"
 reading_lists = "reading_lists"
 USERS = "users"
-CLIENT_ID = "YYafuLHWKYG7Gm6mXqxF9QEEzCQEYDFa"
-CLIENT_SECRET = "9GVbXGOxrAGcYS7MrQfILEqEFRsLb4zg9VQKXA5NHOk2r63eZ_Llto91rxHPVYWS"
+CLIENT_ID = "8JN4et9Mn6XeitXMC69dAAZQdbkiN0HZ"
+CLIENT_SECRET = "mIKk8POyaTlyKs4qfp7fRRcUXPizkyofhqqBZOPCpS6J9HOpHEraJBkM6VAJOyfC"
 ALGORITHMS = ["RS256"]
 DOMAIN = "dev-8l3axw71ej7xustv.us.auth0.com"
